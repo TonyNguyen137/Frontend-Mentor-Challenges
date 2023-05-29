21099a5e14d9e7d2b5b227d1254311612f82d7dd
TonyNguyen137 <tonynguyen137@yahoo.de>
2023-05-29 17:53:14 +0200

    inor bug fix, dropdown project

--- Intro section with dropdown navigation main/src/script.js
+++ Intro section with dropdown navigation main/src/script.js
@@ -1,16 +1,28 @@
 let buttons = document.querySelectorAll("button");
+
+let currentDropdown;
 function toggler(e) {
-    buttons.forEach(e => {
-        if(e === this) return;
-        e.setAttribute("aria-expanded","false");
-    })
+    if(window.innerWidth >= 1024) {
+        buttons.forEach(e => {
+            if(e === this) {
+                currentDropdown = e;
+                return;
+            }
+            e.setAttribute("aria-expanded","false");
+        })
+    } 
+
     const expanded = this.getAttribute("aria-expanded") === "true";
     this.setAttribute("aria-expanded", !expanded)
+
+
 }
 buttons.forEach( e => {
     e.addEventListener("click", toggler);
 
 })
+
+
 
 
 
