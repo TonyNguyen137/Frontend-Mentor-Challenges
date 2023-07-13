9c56532650c76a609a863a300bcb46d846475ab9
Tony Nguyen <tonynguyen@MacBook-Pro-von-Tony.fritz.box>
2023-07-13 21:36:27 +0200

    added newsletter-sign-up-with-success-message-main challenge

---
+++ newsletter-sign-up-with-success-message-main/README.md
@@ -0,0 +1,1 @@
+My first project where I used Bootstrap. It took me 2 days T_T to figure out how bootstrap work and how to customize it.
---
+++ newsletter-sign-up-with-success-message-main/src/assets/js/main.js
@@ -0,0 +1,22 @@
+const form = document.querySelector(".needs-validation");
+const input = document.querySelector("input");
+const email = document.querySelector(".email");
+const successContainer = document.querySelector(".success-message");
+const buttonClose = document.querySelector(".close-success-message-btn");
+const main = document.querySelector(".main");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (!form.checkValidity()) {
+    form.classList.add("was-validated");
+  } else {
+    email.textContent = input.value;
+    successContainer.classList.toggle("d-none");
+    main.classList.toggle("d-md-none");
+  }
+});
+
+buttonClose.addEventListener("click", () => {
+  successContainer.classList.toggle("d-none");
+  main.classList.toggle("d-md-none");
+});
