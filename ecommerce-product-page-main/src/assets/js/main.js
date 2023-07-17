b687ad8d97aae645a13f7b52a96a3b8521c86c67
Tony Nguyen <tonynguyen@MacBook-Pro-von-Tony.fritz.box>
2023-07-17 07:05:06 +0200

    ecommerce project in process

---
+++ ecommerce-product-page-main/src/assets/js/main.js
@@ -0,0 +1,8 @@
+import * as bootstrap from 'bootstrap';
+
+// const menuBtn = document.querySelector('.menu-button');
+
+// menuBtn.addEventListener('click', function () {
+//   this.classList.toggle('change');
+// });
+// console.log('hi');
